75db8fc68c513e9165b3cf3a0f6dad51531590ba
start.vaadin.com <no-reply@vaadin.com>
2023-12-01 14:02:23 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/myview/MyViewView.java
@@ -0,0 +1,74 @@
+package com.example.application.views.myview;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import java.util.ArrayList;
+import java.util.List;
+
+@PageTitle("My View")
+@Route(value = "my-view", layout = MainLayout.class)
+@Uses(Icon.class)
+public class MyViewView extends Composite<VerticalLayout> {
+
+    public MyViewView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        H1 h1 = new H1();
+        ComboBox comboBox = new ComboBox();
+        TextField textField = new TextField();
+        NumberField numberField = new NumberField();
+        Button buttonPrimary = new Button();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        h1.setText("Nuevo Producto");
+        h1.setWidth("max-content");
+        comboBox.setLabel("Tipo");
+        comboBox.setWidth("min-content");
+        setComboBoxSampleData(comboBox);
+        textField.setLabel("Nombre");
+        textField.setWidth("min-content");
+        numberField.setLabel("Cantidad");
+        numberField.setWidth("min-content");
+        buttonPrimary.setText("Button");
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        getContent().add(layoutRow);
+        layoutRow.add(layoutColumn2);
+        layoutColumn2.add(h1);
+        layoutColumn2.add(comboBox);
+        layoutColumn2.add(textField);
+        layoutColumn2.add(numberField);
+        layoutColumn2.add(buttonPrimary);
+    }
+
+    record SampleItem(String value, String label, Boolean disabled) {
+    }
+
+    private void setComboBoxSampleData(ComboBox comboBox) {
+        List<SampleItem> sampleItems = new ArrayList<>();
+        sampleItems.add(new SampleItem("first", "First", null));
+        sampleItems.add(new SampleItem("second", "Second", null));
+        sampleItems.add(new SampleItem("third", "Third", Boolean.TRUE));
+        sampleItems.add(new SampleItem("fourth", "Fourth", null));
+        comboBox.setItems(sampleItems);
+        comboBox.setItemLabelGenerator(item -> ((SampleItem) item).label());
+    }
+}
